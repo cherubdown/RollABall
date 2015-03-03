cba1ab05946f490a2c3b9033001ab570bf66689b
Christopher Bales <radagast1986@yahoo.com>
2015-03-03 08:24:48 -0600

    Init Commit
    This commit includes the basics of the game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+    // physics calculations are applied right before this func
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		// Time.deltaTime ensures framerate smoothness
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+    }
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+		}
+	}
+
+}
